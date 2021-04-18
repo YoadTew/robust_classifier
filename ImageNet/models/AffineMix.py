47ed50c674cd224f2a9141cdde2167b9dd0258ce
YoadTew <yoad.tewel@gmail.com>
2021-04-18 16:43:24 +0300

    Second dropout try

--- ImageNet/models/AffineMix.py
+++ ImageNet/models/AffineMix.py
@@ -9,16 +9,17 @@ class AffineMix(nn.Module):
         self.bn_shape = nn.BatchNorm2d(planes)
         self.bn_color = nn.BatchNorm2d(planes)
 
-        self.convex_weights = nn.Parameter(torch.ones(2) * 0.5)
-        self.dropout = nn.Dropout(p=0.3)
+        self.convex_weights = nn.Parameter(torch.ones(1, 2, 1, 1, 1) * 0.5)
+        self.dropout = nn.Dropout(p=0.1)
 
     def forward(self, x):
 
         x_1 = self.bn_shape(x)
         x_2 = self.bn_color(x)
 
-        weights = self.dropout(self.convex_weights)
+        weights = self.convex_weights.repeat(x.size(0), 1, 1, 1, 1)
+        weights = self.dropout(weights)
 
-        x = weights[0] * x_1 + weights[1] * x_2
+        x = weights[:, 0] * x_1 + weights[:, 1] * x_2
 
         return x
