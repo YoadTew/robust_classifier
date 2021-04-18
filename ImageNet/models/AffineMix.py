4e05efd78de93a5a14e621a7974a37a2011a4d7c
YoadTew <yoad.tewel@gmail.com>
2021-04-18 11:55:35 +0300

    Added dropout to AffineMix again

--- ImageNet/models/AffineMix.py
+++ ImageNet/models/AffineMix.py
@@ -9,16 +9,16 @@ class AffineMix(nn.Module):
         self.bn_shape = nn.BatchNorm2d(planes)
         self.bn_color = nn.BatchNorm2d(planes)
 
-        self.convex_weights = nn.Parameter(torch.ones(2)) * 0.5
-        # self.dropout = nn.Dropout(p=0.3)
+        self.convex_weights = nn.Parameter(torch.ones(2) * 0.5)
+        self.dropout = nn.Dropout(p=0.3)
 
     def forward(self, x):
 
         x_1 = self.bn_shape(x)
         x_2 = self.bn_color(x)
 
-        # weights = self.dropout(self.convex_weights)
+        weights = self.dropout(self.convex_weights)
 
-        x = self.convex_weights[0] * x_1 + self.convex_weights[1] * x_2
+        x = weights[0] * x_1 + weights[1] * x_2
 
         return x
